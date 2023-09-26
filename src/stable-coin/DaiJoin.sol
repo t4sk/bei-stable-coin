fdf9d0677f968496aa2194db136bc8a67d04d401
t4sk <tsk.nakamura@gmail.com>
2023-09-26 12:20:54 +0900

    dai join

--- src/stable-coin/DaiJoin.sol
+++ src/stable-coin/DaiJoin.sol
@@ -4,50 +4,24 @@ pragma solidity 0.8.19;
 import {IVat} from "../interfaces/IVat.sol";
 import {IDai} from "../interfaces/IDai.sol";
 import {RAY} from "../lib/Math.sol";
+import "../lib/Auth.sol";
+import "../lib/Pause.sol";
 
-contract DaiJoin {
-    event AddAuthorization(address indexed user);
-    event RemoveAuthorization(address indexed user);
+contract DaiJoin is Auth, Pause {
     event Join(address indexed user, uint256 wad);
     event Exit(address indexed user, uint256 wad);
-    event Pause();
 
     IVat public immutable vat;
     IDai public immutable dai;
-    bool public live;
-
-    // wards
-    mapping(address => bool) public authorized;
-
-    modifier auth() {
-        require(authorized[msg.sender], "not authorized");
-        _;
-    }
 
     constructor(address _vat, address _dai) {
-        authorized[msg.sender] = true;
-        live = true;
         vat = IVat(_vat);
         dai = IDai(_dai);
-        emit AddAuthorization(msg.sender);
-    }
-
-    // rely
-    function addAuthorization(address user) external auth {
-        authorized[user] = true;
-        emit AddAuthorization(user);
-    }
-
-    // deny
-    function remoteAuthorization(address user) external auth {
-        authorized[user] = false;
-        emit RemoveAuthorization(user);
     }
 
     // cage
     function stop() external auth {
-        live = false;
-        emit Pause();
+        _stop();
     }
 
     function join(address user, uint256 wad) external {
