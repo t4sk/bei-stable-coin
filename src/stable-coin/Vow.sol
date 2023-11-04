b1a1aab7266031b2c266796eaeb36dbd816df950
t4sk <tsk.nakamura@gmail.com>
2023-11-04 09:13:52 +0900

    rename files

--- src/stable-coin/Vow.sol
+++ src/stable-coin/Vow.sol
@@ -8,8 +8,8 @@ import {Math} from "../lib/Math.sol";
 import {Auth} from "../lib/Auth.sol";
 import {Pause} from "../lib/Pause.sol";
 
-// Vow
-contract DebtEngine is Auth, Pause {
+// Vow - Debt engine
+contract Vow is Auth, Pause {
     IVat public immutable vat;
     // flapper
     ISurplusAuctionHouse public surplusAuctionHouse;
